f43bfdf4585adeb0f8aac51d72dd21c55e13c705
Joseph Julicher <jjulicher@mac.com>
2022-12-19 11:01:52 -0700

    removed unused doxyfile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,13 @@ version = '0.1.0'
 
 # -- General configuration
 
-import subprocess
-subprocess.call('cd .. ; doxygen', shell=True)
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+     subprocess.call('cd ../doxygen; doxygen', shell=True)
+     
 
 extensions = [
     'sphinx.ext.duration',
